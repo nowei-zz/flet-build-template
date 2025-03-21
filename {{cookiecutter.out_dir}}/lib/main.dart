b55c9954ddce4ee2f1207674369023c687326e32
nowei-zz <steelztx@126.com>
2025-03-21 18:47:07 +0800

    Update main.dart

--- {{cookiecutter.out_dir}}/lib/main.dart
+++ {{cookiecutter.out_dir}}/lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:flet/flet.dart';
+import 'package:flet/lib/flet.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
